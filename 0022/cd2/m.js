a9818050c355d8abc300252142220b86514176b0
littleflute <13699175164@sina.cn>
2017-07-02 15:50:44 -0500

    Add files via upload

---
+++ 0022/cd2/m.js
@@ -0,0 +1,11 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "The Capital of the World.mp3";		
+f[2]	= "The Capital of the World~1.mp3";		
+f[3]	= "The Capital of the World~2.mp3";		
+f[4]	= "The Capital of the World~3.mp3";		
+f[5]	= "The Capital of the World~4.mp3";		
+f[6]	= "The Capital of the World~5.mp3";		
+f[7]	= "The Capital of the World~6.mp3";		
+f[8]	= "The Capital of the World~7.mp3";		
+f[9]	= "The Capital of the World~8.mp3";		
